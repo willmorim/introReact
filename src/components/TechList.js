63830511dc1a358036d70d8bdf47ef20d2f9b192
willmorim <pdbawilliam@gmail.com>
2019-11-13 00:57:13 -0300

    lifecycle

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -5,8 +5,22 @@ import TechItem from "./TechItem";
 class TechList extends Component {
   state = {
     newTech: "",
-    techs: ["Node.js", "ReactJS", "React Native"]
+    techs: []
   };
+
+  componentDidMount() {
+    const techs = localStorage.getItem("techs");
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem("techs", JSON.stringify(this.state.techs));
+    }
+  }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
